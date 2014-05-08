28e805812510b12a7ee193c2943f3b4bfea340d2
John Colagioia <john@colagioia.net>
2014-05-08 16:21:33 -0400

    Added window-tracking.

---
+++ uManage.h
@@ -0,0 +1,20 @@
+#ifndef U_MANAGE__H
+
+struct window_state {
+    int             force;              /* Exiting, so report anyway */
+    unsigned long   last_idle;          /* Milliseconds of idle time */
+    Window          window_id;
+    unsigned char   window_title[512];
+    time_t          window_start,       /* Use of window started at (s) */
+                    idle_start,         /* Idle started at (s) */
+                    idle_accumulated;   /* Idle with window focus (s) */
+    char            csv[1024];          /* For reporting - output only */
+};
+
+void   handle_break (int signal);
+void   window_state_init (struct window_state *);
+time_t window_state_report (struct window_state *, unsigned char *);
+char * window_state_format (struct window_state *, time_t *, time_t *);
+int    is_window_updated (xdo_t *, struct window_state *);
+
+#endif
