68d53573a93898343766099bd8ef267c2a6e0461
John Colagioia <john@colagioia.net>
2014-07-12 06:30:43 -0400

    Mouse moves correctly

--- mouse.c
+++ mouse.c
@@ -1,5 +1,14 @@
+#include <time.h>
+#include <xdo.h>
 #include "mouse.h"
 
-void move_mouse(void) {
+static xdo_t *xdo;
+static int mouse_move_dir = -25;
 
+void move_mouse(void) {
+    if(xdo == NULL) {
+        xdo = xdo_new(NULL);
+    }
+    xdo_move_mouse_relative(xdo, mouse_move_dir, mouse_move_dir);
+    mouse_move_dir = -mouse_move_dir;
 }
